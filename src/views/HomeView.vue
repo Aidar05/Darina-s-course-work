de9ee5f03b4abd61ecc98c2268e971d043c67c35
Aidar <sharipovaydar200515@gmail.com>
2024-03-08 00:25:03 +0600

    Сделал цитату ту

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="home-main-container">
-    <Header></Header>
+  <Header></Header>
+    
+  <div class="main-container">
+    <article class="quote-container">
+      <p id="quote" v-html="quote"></p>
+
+      <span id="quote-author">{{ quoteAuthor }}</span>
+    </article>
   </div>
 </template>
 
@@ -11,6 +17,14 @@ export default {
   name: 'HomeView',
   components: {
     Header
+  },
+  data(){
+    return{
+      quote: '„Я вложил свое сердце и <br> душу в свою \
+        <span id="highlight-work">работу</span>, \
+        и я <br> сошел с ума в процессе.“',
+      quoteAuthor: "— Винсент Ван Гок"
+    }
   }
 }
 </script>
