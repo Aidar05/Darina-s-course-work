1ef39865278aa4b63b9d5c8a1252b5e0130bc236
Aidar <sharipovaydar200515@gmail.com>
2024-03-07 23:42:54 +0600

    Сделал хэдер

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="header">
-    <router-link to="/">LoGo</router-link>
+    <router-link to="/" class="logo">LOGO</router-link>
     
     <div class="header-menu">
-      <a href="#">Ван Гок</a>
-      <a href="#">История Работ</a>
-      <router-link to="/gallery">Галерея</router-link>
-      <a href="#">Критики</a>
+      <router-link v-for="(element, index) in menuElements" :index="key"  
+        to="element.link" class="menu-element">
+        {{ element.text }}
+      </router-link>
     </div>
   </div>
 </template>
 
+<script>
+export default{
+  name: "Header",
+  data(){
+    return{
+      menuElements: [
+        {text: "Ван Гок", link: "Van-Gock"},
+        {text: "история работ", link: "works"},
+        {text: "галерея", link: "/gallery"},
+        {text: "критики", link: "critics"}
+      ]
+    }
+  }
+}
+</script>
