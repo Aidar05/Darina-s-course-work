4da930c27e94d3d01e71b63de594d2e0a1dc6ba8
Aidar <sharipovaydar200515@gmail.com>
2024-03-07 08:44:17 +0600

    Начал писать сайт курсовую Дарины. Она в обмен сделает дизайн на мой сайт и документ курсовой

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">
+    <router-link to="/">LoGo</router-link>
+    
+    <div class="header-menu">
+      <a href="#">Ван Гок</a>
+      <a href="#">История Работ</a>
+      <router-link to="/gallery">Галерея</router-link>
+      <a href="#">Критики</a>
+    </div>
+  </div>
+</template>
+
