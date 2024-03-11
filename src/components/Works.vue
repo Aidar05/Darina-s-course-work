d39825aac24db351463a324801a24b5becd5baf2
Aidar <sharipovaydar200515@gmail.com>
2024-03-11 13:50:21 +0600

    Сделал эту часть. Надо ток css подправить потом

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,23 +1,21 @@
 <template>
-  <div id="works">
-    <div class="work-element" :class="(bgColor)">
-      <article class="work-text-container">
-        <h1 class="work-header">{{ workHeader }}</h1>
+  <div class="work-element" :class="(bgColor)">
+    <article class="work-text-container">
+      <h1 class="work-header">{{ workHeader }}</h1>
 
-        <p class="work-text">{{ workText }}</p>
-      </article>
-      
-      <div class="work-img-container">
-        <img :src="imgSrc" alt="Resource not found" class="work-img">
+      <p class="work-text">{{ workText }}</p>
+    </article>
+    
+    <div class="work-img-container">
+      <img :src="imgSrc" alt="Resource not found" class="work-img">
 
-        <div class="credit-container">
-          <span class="img-info">{{ imgInfo }}</span>
-          <span class="img-info museum">{{ museum }}</span>
-          <div class="line first-line"></div>
-          <div class="line second-line"></div>
-        </div>
-        
+      <div class="credit-container">
+        <span class="img-info">{{ imgInfo }}</span>
+        <span class="img-info museum">{{ museum }}</span>
+        <div class="line first-line"></div>
+        <div class="line second-line"></div>
       </div>
+      
     </div>
   </div>
 </template>
