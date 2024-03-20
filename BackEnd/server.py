977ea33880f3534144ead62eba08b8adec5f1903
Aidar <sharipovaydar200515@gmail.com>
2024-03-20 17:12:00 +0600

    Соедил фронт с беком. Flask теперь сервит vue.js статичный файл

--- BackEnd/server.py
+++ BackEnd/server.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../FrontEnd/dist')
 
-@app.route('/', methods=["POST", "GET"])
+@app.route('/')
 def main_page():
-  return "Goodbye World!"
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/static/<path:filename>')
+def staticfiles(filename):
+    return send_from_directory(app.static_folder, filename)
+
+if __name__ == "__main__":
+    app.run(debug=True)
